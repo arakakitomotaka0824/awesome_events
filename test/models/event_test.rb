8a9c007e850c6adec272bfe1ef9bd8e832e84d10
新垣友崇 <0824arakaki@gmail.com>
2020-11-09 15:26:56 +0900

    finish 7

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,38 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "#created_by? owner_idと引数の#idが同じ時" do
+    event = FactoryBot.create(:event)
+    user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    assert_equal(true, event.created_by?(user))
+    user.verify
+  end
+
+  test "#created_by? owner_idと引数の#idが異なる時" do
+    event = FactoryBot.create(:event)
+    another_user = FactoryBot.create(:user)
+    assert_equal(false, event.created_by?(another_user))
+  end
+
+  test '#created_by? 引数が nil な時' do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.created_by?(nil))           # ❸
+  end
+
+  test 'start_at_should_be_before_end_at validation OK' do
+    start_at = rand(1..30).days.from_now        # ❶
+    end_at = start_at + rand(1..30).hours       # ❷
+    event = FactoryBot.build(:event, start_at: start_at, end_at: end_at)  # ❸
+    event.valid?                                # ❹
+    assert_empty(event.errors[:start_at])       # ❺
+  end
+
+  test 'start_at_should_be_before_end_at validation error' do
+    start_at = rand(1..30).days.from_now
+    end_at = start_at - rand(1..30).hours
+    event = FactoryBot.build(:event, start_at: start_at, end_at: end_at)
+    event.valid?
+    assert_not_empty(event.errors[:start_at])      # ❶
+  end
 end
