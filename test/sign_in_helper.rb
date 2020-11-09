96577a3ba25b6c620ff0d2b9604b3c012414909f
新垣友崇 <0824arakaki@gmail.com>
2020-11-09 14:58:52 +0900

    finished 7-5

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -7,12 +7,23 @@ module SignInHelper
       info: { nickname: user.name,
               image: user.image_url })
 
-    visit root_url                    # ❹
-    click_on "GitHubでログイン"        # ❺
+    case
+    when respond_to?(:visit)
+      visit root_url                    # ❹
+      click_on "GitHubでログイン"
+    when respond_to?(:get)
+      get "/auth/github/callback"
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
 
   def current_user
     @current_user
   end
+end
+
+class ActionDispatch::IntegrationTest   # ❷
+  include SignInHelper                  # ❸
 end
